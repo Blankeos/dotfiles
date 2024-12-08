82f5a2984dcfd94d80848141282f28cec2ab2ede
Blankeos <carloantonioct@gmail.com>
2024-12-09 04:17:11 +0800

    feat: Updated wezterm centering.

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,7 +1,7 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- THis will hold the configuration.
+-- This will hold the configuration.
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("BlexMono Nerd Font")
@@ -9,14 +9,13 @@ config.font_size = 13
 
 config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.80
 config.macos_window_background_blur = 10
-
-
 config.colors = {
-    background = "rgba(18, 18, 18, 0.82)",
-    cursor_bg = "rgba(186, 230, 253, 1)",
-    selection_bg = "rgba(96, 165, 250, 0.4)",
+
+	background = "rgba(18, 18, 18, 0.82)",
+	cursor_bg = "rgba(186, 230, 253, 1)",
+	selection_bg = "rgba(96, 165, 250, 0.4)",
 }
 
 return config
