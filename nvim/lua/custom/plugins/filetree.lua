127d57c9750bcb1ee50adc257748f5d5c7700b28
Blankeos <carloantonioct@gmail.com>
2025-04-20 22:14:25 +0800

    feat(neovim): Added avante, fixed file tree.

--- nvim/lua/custom/plugins/filetree.lua
+++ nvim/lua/custom/plugins/filetree.lua
@@ -2,18 +2,24 @@
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  config = function ()
-    require('neo-tree').setup {
-      window = {
-        position = 'right',
-      }
-    }
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			window = {
+				position = "right",
+			},
+			filesystem = {
+				follow_current_file = {
+					enabled = true,
+					-- leave_dirs_open = true, -- Optional: Keep parent directories open.
+				},
+			},
+		})
+	end,
 }
