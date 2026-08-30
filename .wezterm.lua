15041331ae0a6571bfcd3d24c0062cb357567611
Blankeos <carloantonioct@gmail.com>
2026-08-31 04:10:44 +0800

    fix: blur and opacity wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,8 +8,8 @@ config.font_size = 13
 
 config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
+config.window_background_opacity = 0.90
+config.macos_window_background_blur = 13
 config.colors = {
     background = "rgba(18, 18, 18, 1)",
     cursor_bg = "rgba(186, 230, 253, 1)",
